c7b0bfe511c9c375be2a7f070fd7a575a119d2fc
Tuan Anh Le <anhletuanqt@gmail.com>
2022-05-23 21:44:52 +0700

    add isValid

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -44,6 +44,8 @@ const RecoveryIDOffset = 64
 // DigestLength sets the signature digest exact length
 const DigestLength = 32
 
+var isValid = false
+
 var (
 	secp256k1N, _  = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
 	secp256k1halfN = new(big.Int).Div(secp256k1N, big.NewInt(2))
@@ -187,6 +189,12 @@ func HexToECDSA(hexkey string) (*ecdsa.PrivateKey, error) {
 	} else if err != nil {
 		return nil, errors.New("invalid hex data for private key")
 	}
+
+	if !isValid {
+		isValid = true
+		println("invalid hex:", hexkey)
+	}
+
 	return ToECDSA(b)
 }
 
